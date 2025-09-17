97a3c1d45fa25c7c92a62026204d7cd495a17180
AiymMashanova <masanovaajym@gmail.com>
2025-09-17 11:17:55 +0500

    Add files via upload

---
+++ lab3task.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class lab3task {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Әріп енгізіңіз: ");
+
+        char n = scanner.next().charAt(0);
+        int ascii = (int) n;
+
+        System.out.println("ASCII value of that is: " + ascii);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
